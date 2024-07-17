4f15b81967e429b3a9b3cd3e6e32e4498e54c5b0
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-07-17 12:35:20 +0700

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
   process.env = loadEnv(mode, process.cwd())
 
   return {
-    base: 'https://freshcart-rust-eta.vercel.app/',
+    base: '/',
     plugins: [
       vue(),
       vueJsx(),
