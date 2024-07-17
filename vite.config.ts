2bc5f46ee06a1574c49574ce85269992584f9f1b
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-07-17 12:03:37 +0700

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
   process.env = loadEnv(mode, process.cwd())
 
   return {
-    base: '/',
+    base: '/freshcart/',
     plugins: [
       vue(),
       vueJsx(),
